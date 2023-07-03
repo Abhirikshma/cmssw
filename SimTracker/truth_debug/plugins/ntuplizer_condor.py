cc9200f7c7897617ac11672f59e9f190f6efe0d3
Abhirikshma <abhirikshma14@gmail.com>
2023-07-03 16:39:38 +0200

    del and add fresh ntuplizer_condor.py

--- SimTracker/truth_debug/plugins/ntuplizer_condor.py
+++ SimTracker/truth_debug/plugins/ntuplizer_condor.py
@@ -18,10 +18,13 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 
-process.load("Geometry.TrackerNumberingBuilder.trackerTopology_cfi")
 process.load("SimTracker.TrackerHitAssociation.tpClusterProducer_cfi")
 process.load("SimGeneral.TrackingAnalysis.simHitTPAssociation_cfi")
 process.load("SimTracker.TrackAssociatorProducers.trackAssociatorByHits_cfi")
+process.load("SimTracker.TrackAssociation.trackingParticleRecoTrackAsssociation_cfi")
+process.load("SimTracker.VertexAssociation.VertexAssociatorByTracks_cfi")
+process.load("RecoTracker.Configuration.RecoTracker_cff")
+process.load("SimTracker.TrackHistory.VertexClassifier_cff")
 
 options = VarParsing.VarParsing('standard')
 options.register('sourcefile', '', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, 'Source file')
@@ -29,23 +32,32 @@ options.parseArguments()
 
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
-                                fileNames = cms.untracked.vstring(
-            #'file:/afs/cern.ch/user/a/abhiriks/CMSSW_12_6_4/src/1325.0_TTbar_13+TTbar_13+DIGIUP15+RECOUP15+HARVESTUP15+ALCATTUP15+NANOUP15/RECO_TrackingPart_DIGI_L1_DIGI2RAW_RAW2DIGI_L1Reco_RECO.root'
-            #'root://xrootd-cms.infn.it//store/relval/CMSSW_12_4_0_pre2/RelValTTbar_14TeV/FEVTDEBUGHLT/123X_upgrade2018_realistic_v2_v2-v1/2580000/20a860f4-2321-449a-bf13-20669bc27fa6.root'
-            options.sourcefile
-            )
+                                fileNames = cms.untracked.vstring(options.sourcefile)
                             )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
 process.TFileService = cms.Service("TFileService", 
             fileName = cms.string("ntuple.root")
     )
 
-process.analyze = cms.EDAnalyzer("Ntuplizer")
+process.trackingParticleRecoTrackAssociationByHits = process.trackingParticleRecoTrackAsssociation.clone(
+    associator = 'trackAssociatorByHits'
+)
+process.vertexAssociatorByTracksByHits = process.VertexAssociatorByTracks.clone(
+    trackAssociation = "trackingParticleRecoTrackAssociationByHits",
+)
+
+process.analyze = cms.EDAnalyzer("Ntuplizer",
+    vertToTvAssociator = cms.InputTag("vertexAssociatorByTracksByHits"),
+    enableDebug = cms.bool(False)
+)
 
 process.p = cms.Path(
     process.simHitTPAssocProducer+
     process.tpClusterProducer+
     process.trackAssociatorByHits+
+    process.trackingParticleRecoTrackAssociationByHits+
+    process.vertexAssociatorByTracksByHits+
     process.analyze
 )
+
