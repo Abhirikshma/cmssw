168455bed8ef4fc8aa030f543ad071433ee85afa
Abhirikshma <abhirikshma14@gmail.com>
2023-07-03 17:59:38 +0200

    all events

--- SimTracker/truth_debug/plugins/ntuplizer_condor.py
+++ SimTracker/truth_debug/plugins/ntuplizer_condor.py
@@ -34,7 +34,7 @@ process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
                                 fileNames = cms.untracked.vstring(options.sourcefile)
                             )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.TFileService = cms.Service("TFileService", 
             fileName = cms.string("ntuple.root")
